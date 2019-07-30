c14d7301a7b25cbdab9b7828cee4f45992af3caa
Dennis Timmers <dennistimmers@burst-digital.com>
2019-07-30 20:24:46 +0200

    feat(setup): add react three fiber

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import * as THREE from 'three'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Canvas } from 'react-three-fiber'
 
-function App() {
+function Thing({ vertices }) {
   return (
-    <div className="App"></div>
-  );
+    <group ref={ref => console.log('we have access to the instance')}>
+      <line>
+        <geometry
+          attach="geometry"
+          vertices={vertices.map(v => new THREE.Vector3(...v))}
+          onUpdate={self => (self.verticesNeedUpdate = true)}
+        />
+        <lineBasicMaterial attach="material" color="black" />
+      </line>
+      <mesh
+        onClick={e => console.log('click')} 
+        onPointerOver={e => console.log('hover')} 
+        onPointerOut={e => console.log('unhover')}>
+        <octahedronGeometry attach="geometry" />
+        <meshBasicMaterial attach="material" color="peachpuff" opacity={0.5} transparent />
+      </mesh>
+    </group>
+  )
 }
 
-export default App;
+ReactDOM.render(
+  <Canvas>
+    <Thing vertices={[[-1, 0, 0], [0, 1, 0], [1, 0, 0], [0, -1, 0], [-1, 0, 0]]} />
+  </Canvas>,
+  document.getElementById('root')
+)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as THREE from 'three'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Canvas } from 'react-three-fiber'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Thing({ vertices }) {
+  return (
+    <group ref={() => console.log('we have access to the instance')}>
+      <line>
+        <geometry
+          attach="geometry"
+          vertices={vertices.map(v => new THREE.Vector3(...v))}
+          onUpdate={self => (self.verticesNeedUpdate = true)}
+        />
+        <lineBasicMaterial attach="material" color="black" />
+      </line>
+      <mesh
+        onClick={() => console.log('click')} 
+        onPointerOver={() => console.log('hover')} 
+        onPointerOut={() => console.log('unhover')}>
+        <octahedronGeometry attach="geometry" />
+        <meshBasicMaterial attach="material" color="red" opacity={.5} />
+      </mesh>
+    </group>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Canvas>
+    <Thing vertices={[[1, 0, 0], [0, 1, 0], [1, 0, 0], [0, -1, 0], [-1, 0, 0]]} />
+  </Canvas>,
+  document.getElementById('root')
+)
